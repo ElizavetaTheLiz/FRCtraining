781ef1105e85b2629f600c6f357aa45f9fabe56c
Elizaveta G <elizavetahry@gmail.com>
2021-04-30 20:16:23 +0300

    Update RobotContainer.java

--- command_based2021/RobotContainer.java
+++ command_based2021/RobotContainer.java
@@ -1,48 +1,38 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.GenericHID;
-import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.commands.autoCommand;
+import frc.robot.commands.redlineCommand;
+import frc.robot.subsystems.drivetrainSubsystem;
+import frc.robot.subsystems.encoderSubsystem;
+import frc.robot.subsystems.joystickSubsystem;
+import frc.robot.subsystems.redlineSubsystem;
+import frc.robot.subsystems.ultrasonicSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 
-/**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
- * subsystems, commands, and button mappings) should be declared here.
- */
+
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  private drivetrainSubsystem drive      = drivetrainSubsystem.getInstance();
+  private encoderSubsystem encoder       = encoderSubsystem.getInstance();
+  private redlineSubsystem redline       = redlineSubsystem.getInstance();
+  private ultrasonicSubsystem ultrasonic = ultrasonicSubsystem.getInstance();
+  private joystickSubsystem joystick     = joystickSubsystem.getInstance();
+  private autoCommand auto               = new autoCommand(drive, encoder, ultrasonic);
+  private redlineCommand redlineCommand  = new redlineCommand(redline);
 
-  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+
   public RobotContainer() {
-    // Configure the button bindings
     configureButtonBindings();
   }
 
-  /**
-   * Use this method to define your button->command mappings. Buttons can be created by
-   * instantiating a {@link GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
-   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
-   */
-  private void configureButtonBindings() {}
+  private void configureButtonBindings() {
+    joystick.button.whileActiveContinuous(redlineCommand);
+  }
 
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
+
   public Command getAutonomousCommand() {
-    // An ExampleCommand will run in autonomous
-    return m_autoCommand;
+    return auto;
   }
 }
